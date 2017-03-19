6243872ad1c24e8242e06cb6549406b6a260afd0
Olle Harstedt <olleharstedt@yahoo.com>
2017-03-19 21:26:56 +0100

    JSDoc of version class

--- scripts/civclicker-classes.js
+++ scripts/civclicker-classes.js
@@ -1,13 +1,34 @@
-
+/**
+ * Class for version.
+ *
+ * @param {number} major
+ * @param {number} minor
+ * @param {number} sub
+ * @param {string} mod
+ */
 function VersionData(major,minor,sub,mod) {
 	this.major = major;
 	this.minor = minor;
 	this.sub = sub;
 	this.mod = mod;
 }
-VersionData.prototype.toNumber = function() { return this.major*1000 + this.minor + this.sub/1000; };
-VersionData.prototype.toString = function() { return String(this.major) + "." 
-	+ String(this.minor) + "." + String(this.sub) + String(this.mod); };
+
+/**
+ * Returns number representation of number.
+ * @return {number}
+ */
+VersionData.prototype.toNumber = function() {
+  return this.major*1000 + this.minor + this.sub/1000;
+};
+
+/**
+ * Returns string of version.
+ * @return {string}
+ */
+VersionData.prototype.toString = function() {
+  return String(this.major) + "."
+    + String(this.minor) + "." + String(this.sub) + String(this.mod);
+};
 
 // TODO: Create a mechanism to automate the creation of a class hierarchy,
 // specifying base class, shared props, instance props.
